c23a1ea0b5b08f4d2b948afe9e58d6b6a7a4ec6d
Daniil Borovkov <daniilborowkow@yandex.ru>
2018-01-06 17:39:40 +0600

    Connected bootstrap4

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import './src/js/test';
+import './node_modules/popper.js/dist/umd/popper.min';
+import './node_modules/bootstrap/dist/js/bootstrap.min';
 // styles
 import './node_modules/bootstrap/scss/bootstrap.scss';
 import './src/scss/base.scss';
